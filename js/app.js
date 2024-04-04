ef68fbaa8ebd889718511918cc96c42c69f6076d
Anwar-Abdel <anwarabdeljaber0@gmail.com>
2024-04-04 00:30:24 -0500

    feat: add styling for menu icon and navbar for 768 pixels and toggle functionality

--- js/app.js
+++ js/app.js
@@ -1,4 +1,11 @@
 /*==================== toggle icon navbar ====================*/
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onClick = () => {
+  menuIcon.classList.toggle('bx-x');
+  navbar.classList.toggle('actove');
+}
 /*==================== scroll sections active link ====================*/
 
 /*==================== sticky navbar ====================*/
@@ -9,7 +16,8 @@ header.classList.toggle('sticky', window.scrollY > 100);
 
 
 /*==================== remove toggle icon and navbar when click navbar link (scroll) ====================*/
-
+menuIcon.classList.remove('bx-x');
+navbar.classList.remove('active');
 /*==================== scroll reveal ====================*/
 /*==================== typed js ====================*/
 /*
