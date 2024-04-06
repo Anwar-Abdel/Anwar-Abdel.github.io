fd7858282b7f1d585d634a6cb3caaa2dfc795f28
Anwar-Abdel <anwarabdeljaber0@gmail.com>
2024-04-06 14:57:01 -0500

    feat: add typed js

--- js/app.js
+++ js/app.js
@@ -32,6 +32,17 @@ ScrollReveal().reveal('.home-content h1, .about-img', {origin: 'left'});
 ScrollReveal().reveal('.home-content p, .about-content', {origin: 'right'});
 
 /*==================== typed js ====================*/
+const typed = new Typed('.multiple-text', {
+  strings: ['Frontend Engineer', 'Backend Engineer', 'Fullstack Engineer'],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true
+});
+
+
+
+
 /*
 // Header data
 const headerData = {
